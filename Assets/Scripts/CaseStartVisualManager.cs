b26d9797993fef461dd629b7726c02dc73513512
Reagan <reaganhenke@gmail.com>
2026-08-19 20:48:44 -0400

    add TODO - SOUND notes

--- Assets/Scripts/CaseStartVisualManager.cs
+++ Assets/Scripts/CaseStartVisualManager.cs
@@ -4,27 +4,40 @@ using UnityEngine.UIElements;
 
 public class CaseStartVisualManager : MonoBehaviour
 {
-
     public UIDocument caseStartUIDoc;
     private VisualElement caseStartUI;
 
     public void OnEnable()
     {
         caseStartUI = caseStartUIDoc.rootVisualElement;
+        caseStartUI.Q<Button>("StartCase").RegisterCallback<PointerEnterEvent>(OnButtonHover);
+    }
+
+    public void OnDisable()
+    {
+        caseStartUI.Q<Button>("StartCase").UnregisterCallback<PointerEnterEvent>(OnButtonHover);
     }
 
     public Task DisplayStartCase(CaseSO caseToStart)
     {
+        // TODO - SOUND :  This is where the initial case popup opens. It will open at the start of each case. 
         var tcs = new TaskCompletionSource<bool>();
         caseStartUI.dataSource = caseToStart;
         caseStartUI.Q<VisualElement>("Overlay").style.display = DisplayStyle.Flex;
         caseStartUI.Q<Button>("StartCase").clicked += ClickedStart;
         void ClickedStart()
         {
+            // TODO - SOUND :  The "Start Case" button is clicked and the popup goes away.  
             caseStartUI.Q<Button>("StartCase").clicked -= ClickedStart;
             caseStartUI.Q<VisualElement>("Overlay").style.display = DisplayStyle.None;
             tcs.TrySetResult(true); // Wait until button is clicked
         }
         return tcs.Task;
+    }
+
+    public void OnButtonHover(PointerEnterEvent evt)
+    {
+        // TODO - SOUND :  The "start case" button was hovered over
+        return;
     }
 }
