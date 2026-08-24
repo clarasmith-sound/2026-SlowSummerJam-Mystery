66139c5170f683ab20506bfbfc25d6de47a60fa1
unknown <schuurmanashley@gmail.com>
2026-08-24 12:05:30 -0700

    Folder sprites; main menu sprites; add to scenes

---
+++ Assets/Art/TitleBob.cs
@@ -0,0 +1,18 @@
+using PrimeTween;
+using UnityEngine;
+
+public class TitleBob : MonoBehaviour
+{
+    void Start()
+    {
+        Move();
+    }
+
+    void Move()
+    {
+        float endTarget = gameObject.transform.position.y + 0.15f;
+
+        Sequence.Create(cycles: -1, cycleMode: CycleMode.Yoyo)
+            .Group(Tween.PositionY(gameObject.transform, endValue: endTarget, duration: 1f, ease: Ease.InOutSine));
+    }
+}
