ee0289fa6c3b4573de69c8b5b25ee173ebd3b26f
Reagan <reaganhenke@gmail.com>
2026-08-30 17:11:51 -0400

    fix zoom bug, optionsMenuOpen bug, attempt to fix confetti on itch

--- Assets/Scripts/CaseFileVisualManager.cs
+++ Assets/Scripts/CaseFileVisualManager.cs
@@ -1,21 +1,29 @@
 using PrimeTween;
 using UnityEngine;
 using UnityEngine.UIElements;
+using FMODUnity;
 
 public class CaseFileVisualManager : MonoBehaviour
 {
     public UIDocument caseFileUIDoc;
     private VisualElement caseFileUI;
 
+    [Header("Audio")]
+    [SerializeField] private EventReference buttonHoverSound;
+    [SerializeField] private EventReference folderOpenSound;
+    [SerializeField] private EventReference folderCloseSound;
+
     public void OnEnable()
     {
         caseFileUI = caseFileUIDoc.rootVisualElement;
         caseFileUI.Q<Button>("ContinueButton").clicked += CloseCaseFile;
+        caseFileUI.Q<Button>("ContinueButton").RegisterCallback<PointerEnterEvent>(OnButtonHover);
     }
 
     public void OnDisable()
     {
         caseFileUI.Q<Button>("ContinueButton").clicked -= CloseCaseFile;
+        caseFileUI.Q<Button>("ContinueButton").UnregisterCallback<PointerEnterEvent>(OnButtonHover);
     }
 
     public void OpenCaseFile(Clue[] clues)
@@ -28,11 +36,19 @@ public class CaseFileVisualManager : MonoBehaviour
         Sequence.Create(cycles: 1)
            .Group(caseFileUI.VisualElementShakeScale(new ShakeSettings(strength: new Vector3(.1f, .1f, .1f), duration: 0.3f, frequency: 3)))
            .Group(caseFileUI.VisualElementPunchRotation(new ShakeSettings(strength: new Vector3(0f, 0f, -5f), duration: 0.25f, frequency: 5)));
+        AudioManager.Instance.PlaySound2D(folderOpenSound);
     }
 
     public void CloseCaseFile()
     {
         if (GameManager.Instance != null) GameManager.Instance.optionsMenuOpen = false;
         caseFileUI.Q<VisualElement>("CaseFileMenu").style.display = DisplayStyle.None;
+        AudioManager.Instance.PlaySound2D(folderCloseSound);
+    }
+
+    public void OnButtonHover(PointerEnterEvent evt)
+    {
+        AudioManager.Instance.PlaySound2D(buttonHoverSound);
+        return;
     }
 }
--- Assets/Scripts/CaseStartVisualManager.cs
+++ Assets/Scripts/CaseStartVisualManager.cs
@@ -3,6 +3,7 @@ using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.UIElements;
 using FMODUnity;
+using PrimeTween;
 
 public class CaseStartVisualManager : MonoBehaviour
 {
@@ -29,16 +30,19 @@ public class CaseStartVisualManager : MonoBehaviour
     {
         // SOUND :  This is where the initial case popup opens. It will open at the start of each case. 
         AudioManager.Instance.PlaySound2D(caseOpenSound);
-        if(GameManager.Instance != null) GameManager.Instance.optionsMenuOpen = true;
+        if (GameManager.Instance != null) GameManager.Instance.optionsMenuOpen = true;
         var tcs = new TaskCompletionSource<bool>();
         caseStartUI.dataSource = caseToStart;
         caseStartUI.Q<VisualElement>("DocumentRoot").style.display = DisplayStyle.Flex;
+        PrimeTween.Sequence.Create(cycles: 1)
+          .Group(caseStartUI.VisualElementShakeScale(new ShakeSettings(strength: new Vector3(.1f, .1f, .1f), duration: 0.3f, frequency: 3)))
+          .Group(caseStartUI.VisualElementPunchRotation(new ShakeSettings(strength: new Vector3(0f, 0f, -5f), duration: 0.25f, frequency: 5)));
         caseStartUI.Q<Button>("StartCase").clicked += ClickedStart;
         void ClickedStart()
         {
             // SOUND :  The "Start Case" button is clicked and the popup goes away.  
             AudioManager.Instance.PlaySound2D(caseStartSound);
-            if(GameManager.Instance != null) GameManager.Instance.optionsMenuOpen = false;
+            if (GameManager.Instance != null) GameManager.Instance.optionsMenuOpen = false;
             caseStartUI.Q<Button>("StartCase").clicked -= ClickedStart;
             caseStartUI.Q<VisualElement>("DocumentRoot").style.display = DisplayStyle.None;
             tcs.TrySetResult(true); // Wait until button is clicked
